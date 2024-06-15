4f3dff93c845dc80b4ed417b66febc7110eac402
joycefgoncalves <goncalvessjoyce@outlook.com>
2024-06-14 19:35:45 -0500

    ecommerce_app

---
+++ ecommerce_app_sql.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE ecommerce_app;
+
+Connection Name: ecommerce_app
+Connection Method: Standard TCP/IP
+Hostname: localhost
+Port: 3306
+Username: root (or your MySQL username)
+Password: ******** (your MySQL password)
+Default Schema: ecommerce_db (the name of your MySQL database)
